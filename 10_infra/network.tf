95f058dd7226a63bc0866e1c54378f0a1ef9f935
tatsuya1995 <tatsuyawada1995@gmail.com>
2024-06-23 21:36:21 +0900

    テラフォームフォーマッタをかける

--- 10_infra/network.tf
+++ 10_infra/network.tf
@@ -2,11 +2,11 @@
 # VPC
 # ---------------------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_blockxx                       = "192.168.0.0/20"
-  instance_tenancy                   = "default"
-  enable_dns_support                 = true
-  enable_dns_hostnames               = true
-  assign_generated_ipv6_cidr_block   = false
+  cidr_blockxx                     = "192.168.0.0/20"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = false
 
   tags = {
     Name    = "${var.project}-${var.environment}-vpc"
